2813062ca92dc76ee7c0e4968021b49c8250068d
Patryk <sunnyflail@gmail.com>
2021-04-23 01:09:35 +0200

    Todo: tests

--- src/Container.php
+++ src/Container.php
@@ -6,11 +6,10 @@ use \Psr\Container\{
     ContainerInterface,
     ContainerExceptionInterface
 };
-
 use \ReflectionClass;
-use \ReflectionFunctionAbstract;
 use \ReflectionMethod;
-
+use \ReflectionException;
+use \ReflectionFunctionAbstract;
 
 class Container implements ContainerInterface
 {
@@ -19,16 +18,15 @@ class Container implements ContainerInterface
 
     private array $entries;
 
-    public function __construct(array $entries = [])
+    public function __construct()
     {
-        $this->entries = $entries;
+        $this->entries = [];
     }
 
     public function get(string $id)
     {
         if (!isset($this->entries[$id])) {
-            throw new NotFoundException(sprintf(
-                "Entry '%s' not found!", $id));
+            throw new NotFoundException(sprintf("Entry '%s' not found!", $id));
         }
         
         if ($this->entries[$id] instanceof Entry) {
@@ -38,7 +36,6 @@ class Container implements ContainerInterface
                 throw new ContainerException($e->getMessage());
             }
         }
-
         return $this->entries[$id];
     }
 
@@ -59,7 +56,7 @@ class Container implements ContainerInterface
 
         if (!class_exists($className)) {
             throw new ContainerException(
-                sprintf("Class '%s' doesn't exist!"));
+                sprintf("Class %s doesn't exist!"));
         }
 
         $reflection = new ReflectionClass($className);       
@@ -74,17 +71,28 @@ class Container implements ContainerInterface
                 sprintf("Something went wrong during instation of %s", $className));
         }
         if (isset($config["methods"])) {
-
+            // Methods to be called just after initialisation
             foreach ($config["methods"] as $methodName => $calls) {
-                foreach ($calls as $callTry => $params) {
-                    
+                try {
+                    $method = $reflection->getMethod($methodName);
+                } catch (\ReflectionException $e) {
+                    throw new ContainerException(
+                        sprintf("Method %s::%s doesn't exist!", ));
                 }
-
-
+                foreach ($calls as $attemptNum => $params) {
+                    $params = $this->resolveFunctionParams($method, $params);
+                    try{
+                        $method->invoke($object, ...$params);
+                    } catch (\ReflectionException) {
+                        throw new ContainerException(
+                            sprintf("Something went wrong during invoking %s::%s for the %s time!",
+                                    $className, $methodName, $attemptNum));
+                    }
+                }
             }
-
         }
-
+    
+        return $object;
     }
 
     private function resolveFunctionParams(
@@ -100,17 +108,30 @@ class Container implements ContainerInterface
                     sprintf("Method %s::%s doesn't take in any parameters!", $className, $methodName));
         }   
 
-        foreach ($params as $param) {
-            $paramName = $param->getName();
-            $paramTypes = $this->getTypeStrings($param);
+       //foreach ($params as $param) {
+        return array_map(
+            function($param) {
+                $paramName = $param->getName();
+                $requiredTypes = $this->getTypeStrings($param);
 
-            if (!$param->isDefaultValueAvailable() && !isset($config[$paramName])) {
-                throw new ContainerException(
-                    sprintf("Value not provided for parameter %s in %s::%s!", $paramName, $className, $methodName));
-            }
+                if (isset($config[$paramName])) {
+                    $argument = $config[$paramName];
+                    
 
+                }
+                if (!$param->isDefaultValueAvailable() && !isset($config[$paramName])) {
+                    throw new ContainerException(
+                        sprintf("Value not provided for parameter %s in %s::%s!", $paramName, $className, $methodName));
+                }
 
-        }
+                return $param->getDefaultValue();
+            },
+        $params);
 
+    }
+
+    public function configure(IContainerLoader $loader)
+    {
+        $this->entries = $loader->loadEntries();
     }
 }
--- t.php
+++ t.php
@@ -4,37 +4,14 @@ require_once "vendor/autoload.php";
 
 use SunnyFlail\DI\Entry;
 
-$c = new class {
+class str
+{
+    public function __construct(public int $int) {}
 
-    use SunnyFlail\Traits\GetTypesTrait;
-
-    public function getIt() {
-
-        $reflection = new ReflectionClass(Entry::class);
-
-        $i = $reflection->getMethod("invoke");
-        $ps = $i->getParameters();
-
-        foreach ($ps as $p) {
-            $t = $this->getTypeStrings($p);
-            var_dump($t);
-        }
-
-    }
-
-};
-
-$fn = function () {
-    echo "DECHO";
-};
+}
 
 
-$ref = new ReflectionFunction($fn);
+$e = new str("123");
 
-var_dump($ref->getClosureThis());
 
-echo PHP_EOL, PHP_EOL;
-
-$refe = new ReflectionClass($c);
-$fere = $refe->getMethod("getIt");
-echo($fere->getDeclaringClass());
+$ref = new ReflectionClass("string");
